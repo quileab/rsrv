bff8465813875d7b59ac2bf61f53937567e99f9d
quileab <quileab@gmail.com>
2022-02-07 17:28:03 -0300

    First Commit

---
+++ database/migrations/2022_02_07_135827_create_equipment_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',60);
+            $table->string('description',250)->nullable();
+            $table->string('serial_number',60)->nullable();
+            $table->string('model',60)->nullable();
+            $table->string('manufacturer',60)->nullable();
+            $table->string('location',60)->nullable();
+            $table->string('image_path', 2048)->nullable();
+            $table->decimal('price',10,2)->nullable();
+            $table->string('status',10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+}
---
+++ database/migrations/2022_02_07_140242_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('equipment_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('status',10)->default('pending');
+            $table->decimal('price',10,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
